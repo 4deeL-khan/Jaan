dee69fe61f2e674d60d250828d18cd35c5d32afa
4deeL-khan <110104681+4deeL-khan@users.noreply.github.com>
2023-05-06 15:25:23 +0500

    Create Love .py

---
+++ Love .py
@@ -0,0 +1,9 @@
+os.system('clear')
+    logo()
+    print ''
+    print '\t\x1b[98;1m  WELCOM TO LEGHARI GROUP'
+    print ''
+    print '\x1b[99;1m  [1] NO ID LOND LO APP'
+    print '\x1b[98;1m  [2] TERIII MA KI XHUUT DOST'
+    print '\x1b[88;1m  [3] ZUKOOO TERII MA KI KALI XHUT '
+    print ''
